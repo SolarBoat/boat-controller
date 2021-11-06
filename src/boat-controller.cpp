506331a495a83c96a699bbb7c72d8834814bbca2
TobiasBodewig <tbodewig@web.de>
2021-11-06 22:06:19 +0100

    use websockets to test parameter framework

--- src/boat-controller.cpp
+++ src/boat-controller.cpp
@@ -11,16 +11,22 @@
  * Date:
  */
 
+#include <WebSocketsClient.h>
+
 #include "parameter/BaseParameter.h"
 #include "parameter/NumParameter.h"
 
 void setup();
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t lenght);
+void handleCommand(char * command);
 void paraChange();
 void loop();
-#line 11 "c:/Users/Tobias/Documents/Projekte/Solarboat/boat-controller/src/boat-controller.ino"
-BaseParameter<bool> para1("Para1", true);
-NumParameter<int> para2("Para2", 42);
-NumParameter<float> para3("Para3", 3.14);
+#line 13 "c:/Users/Tobias/Documents/Projekte/Solarboat/boat-controller/src/boat-controller.ino"
+WebSocketsClient webSocket;
+
+BaseParameter<bool> para1("para-bool", true);
+NumParameter<int> para2("para-int", 42);
+NumParameter<float> para3("para-float", 3.14);
 
 // setup() runs once, when the device is first turned on.
 void setup() {
@@ -32,29 +38,72 @@ void setup() {
   para3.set_on_change(paraChange);
 
   paraChange();
+
+  Serial.println("Connect ...");
+
+  webSocket.begin("82.165.125.185", 80, "/ws/boat/");
+  webSocket.onEvent(webSocketEvent);
+
+  Serial.println("Ready");
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t lenght) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[WSc] Disconnected!\n");
+      break;
+    case WStype_CONNECTED:
+      Serial.printf("[WSc] Connected to url: %s\n",  payload);
+      webSocket.sendTXT("Connected");
+      break;
+    case WStype_TEXT:
+      Serial.printf("[WSc] get text: %s\n", payload);
+      handleCommand((char*)payload);
+      break;
+    case WStype_BIN:
+      Serial.printf("[WSc] get binary lenght: %u\n", lenght);
+      break;
+  }
+}
+
+void handleCommand(char * command) {
+  String com(command);
+  if (com.startsWith("p ") || com.startsWith("parameter ")) {
+    com = com.substring(com.indexOf(' ') + 1);
+    if (com.startsWith("get ")) {
+      com = com.substring(com.indexOf(' ') + 1);
+      String value = Parameter::get_parameter(&com);
+      com = com + ": " + value;
+      webSocket.sendTXT(com);
+    } else if (com.startsWith("set ")) {
+      com = com.substring(com.indexOf(' ') + 1);
+      int index = com.indexOf(' ');
+      if (index == -1) {
+        return;
+      }
+      String param = com.substring(0,index);
+      String value = com.substring(index + 1);
+      Parameter::set_parameter(&param, &value);
+      webSocket.sendTXT("OK");
+    }
+  }
 }
 
 void paraChange() {
   Serial.println("Parameter Changed:");
-  Serial.print("Para1: ");
+  Serial.print("para-bool: ");
   Serial.println(para1.get_value());
-  Serial.print("Para2: ");
+  Serial.print("para-int: ");
   Serial.println(para2.get_value());
-  Serial.print("Para3: ");
+  Serial.print("para-float: ");
   Serial.println(para3.get_value());
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  String p("Para2");
-  String v1("42");
-  String v2("12");
-  Parameter::set_parameter(&p, &v2);
-  Serial.print("Result: ");
-  Serial.println(Parameter::get_parameter(&p));
-  delay(1000);
-  Parameter::set_parameter(&p, &v1);  
-  Serial.print("Result: ");
-  Serial.println(Parameter::get_parameter(&p));
-  delay(1000);
+    for(int i = 0; i < 500; i++) {
+        webSocket.loop();
+        delay(20);
+    }
+    webSocket.sendTXT("ping");
 }
